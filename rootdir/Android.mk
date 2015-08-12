dfbefa5aa8a34e9a8bbdb5e25f3f527a2e8e75ed
Andreas Blaesius <skate4life@gmx.de>
2015-08-12 19:46:01 +0200

    common: move init.recovery.espresso.rc to espresso-common (2/2)
    TARGET_RECOVERY_DEVICE_DIRS can only be defined once, else the build errors out. 
    
    Change-Id: I71cca0054f1edf9a9950f822805c12f478b48497

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -59,3 +59,13 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := ../recovery/root/etc/twrp.fstab
 LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/etc
 include $(BUILD_PREBUILT)
+
+ifneq ($(filter p3100 p3110,$(TARGET_DEVICE)),)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.recovery.espresso.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := ../recovery/root/init.recovery.espresso.rc
+LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)
+include $(BUILD_PREBUILT)
+endif
