5e3ba27382ac56b9069fe5e3477931fba31f0e92
Ziyan <jaraidaniel@gmail.com>
2015-10-17 19:50:15 +0200

    espresso-common: remove TARGET_USE_KERNEL_PVR define
    We always build it as a module instead.
    
    Change-Id: I99cdfa760c27774bf01ccc80499498005921e7d4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,8 +18,6 @@
 # This variable is set first, so it can be overridden
 # by BoardConfigVendor.mk
 
-TARGET_USE_KERNEL_PVR := true
-
 # Inherit common omap4 board config
 -include hardware/ti/omap4/BoardConfigCommon.mk
 
@@ -40,7 +38,6 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_BASE := 0x40000000
 BOARD_KERNEL_CMDLINE :=
 
-ifneq ($(TARGET_USE_KERNEL_PVR),true)
 # External SGX Module
 SGX_MODULES:
 	make clean -C $(HARDWARE_TI_OMAP4_BASE)/pvr-source/eurasiacon/build/linux2/omap4430_android
@@ -50,7 +47,6 @@ SGX_MODULES:
 	$(ARM_EABI_TOOLCHAIN)/arm-eabi-strip --strip-unneeded $(KERNEL_MODULES_OUT)/pvrsrvkm_sgx540_120.ko
 
 TARGET_KERNEL_MODULES += SGX_MODULES
-endif
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
