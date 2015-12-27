30f5283edb7747d7898cf8a85cf5630c0763220f
Andreas Blaesius <skate4life@gmx.de>
2015-12-27 08:17:34 -0800

    espresso-common: remove useless ifdefs
    we're aiming to commonize all variants and this won't hurt wifi-only
    devices
    
    Change-Id: If72d2a12a3fe0268e52bdd5655fcc0edac69a78e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -22,9 +22,7 @@ TARGET_USE_KERNEL_PVR := true
 # Inherit common omap4 board config
 -include hardware/ti/omap4/BoardConfigCommon.mk
 
-ifneq ($(filter p3100 p5100,$(TARGET_DEVICE)),)
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/espresso-common/include
-endif
 
 TARGET_NO_BOOTLOADER := true
 TARGET_NO_RADIOIMAGE := true
@@ -171,10 +169,8 @@ TW_MAX_BRIGHTNESS := 255
 TW_NO_CPU_TEMP := true
 TW_EXCLUDE_MTP := true
 
-ifneq ($(filter p3100 p5100,$(TARGET_DEVICE)),)
 # RIL
 BOARD_VENDOR := samsung
 BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm6260
 BOARD_RIL_CLASS := ../../../device/samsung/espresso-common/ril
-endif
