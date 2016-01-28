68383a3fe65df126f74585adc43084bb060c65d2
Ziyan <jaraidaniel@gmail.com>
2016-01-28 22:47:50 +0100

    rootdir: remove espresso/espresso10 distinction

--- espresso-common.mk
+++ espresso-common.mk
@@ -28,13 +28,9 @@ PRODUCT_AAPT_PREF_CONFIG := mdpi
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.espresso \
-    fstab.espresso10 \
     init.espresso.usb.rc \
-    init.espresso10.usb.rc \
     init.espresso.rc \
-    init.espresso10.rc \
-    ueventd.espresso.rc \
-    ueventd.espresso10.rc
+    ueventd.espresso.rc
 
 # GPS
 # gps config appropriate for this device
