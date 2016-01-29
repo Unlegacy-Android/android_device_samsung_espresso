23ba9a91eb318403ba63bc5cf855afd9eb163d69
Ziyan <jaraidaniel@gmail.com>
2016-01-29 19:19:51 +0100

    Set up unified build for wifi-only variants

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,7 +20,7 @@
 # Inherit common omap4 board config
 -include hardware/ti/omap4/BoardConfigCommon.mk
 
-TARGET_SPECIFIC_HEADER_PATH += device/samsung/espresso-common/include
+TARGET_SPECIFIC_HEADER_PATH += device/samsung/espresso/include
 
 TARGET_NO_BOOTLOADER := true
 
@@ -77,12 +77,12 @@ WIFI_BAND                        := 802_11_ABG
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/espresso-common/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/espresso-common/bluetooth/vnd_espresso.txt
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/espresso/bluetooth
+BOARD_BLUEDROID_VENDOR_CONF := device/samsung/espresso/bluetooth/vnd_espresso.txt
 
 # Selinux
 BOARD_SEPOLICY_DIRS += \
-    device/samsung/espresso-common/sepolicy
+    device/samsung/espresso/sepolicy
 
 BOARD_SEPOLICY_UNION += \
     bluetooth.te \
@@ -105,9 +105,9 @@ BOARD_SEPOLICY_UNION += \
 # Recovery
 TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 BOARD_SUPPRESS_EMMC_WIPE := true
-TARGET_RECOVERY_FSTAB := device/samsung/espresso-common/rootdir/etc/fstab.tab2
+TARGET_RECOVERY_FSTAB := device/samsung/espresso/rootdir/etc/fstab.tab2
 RECOVERY_FSTAB_VERSION := 2
 
 # RIL
 BOARD_PROVIDES_LIBRIL := true
-BOARD_RIL_CLASS := ../../../device/samsung/espresso-common/ril
+BOARD_RIL_CLASS := ../../../device/samsung/espresso/ril
