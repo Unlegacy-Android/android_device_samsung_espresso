0a1973c9526f50544b490cf09c15226501b68f17
Ziyan <jaraidaniel@gmail.com>
2016-09-04 18:44:18 +0200

    Remove OMAP4 common libpower and properties

--- espresso-common.mk
+++ espresso-common.mk
@@ -72,7 +72,6 @@ PRODUCT_PACKAGES += \
     e2fsck \
     lights.piranha \
     libinvensense_mpl \
-    power.piranha \
     sensors.piranha \
     geomagneticd \
     orientationd
@@ -118,7 +117,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=131072 \
-    ro.bq.gpu_to_cpu_unsupported=1 \
     ro.sf.lcd_density=160
 
 PRODUCT_TAGS += dalvik.gc.type-precise
