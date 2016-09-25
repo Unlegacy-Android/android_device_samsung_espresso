39a77a5db75d85f4f889df61a21210f512c6a09c
Ziyan <jaraidaniel@gmail.com>
2016-09-25 12:44:24 +0200

    libsensors: convert to C++
    Change-Id: Ia6c4a6091ef44199f764e9d60837e092788a2d99

--- device-common.mk
+++ device-common.mk
@@ -90,8 +90,7 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     lights.omap4 \
-    libinvensense_mpl \
-    sensors.piranha \
+    sensors.omap4 \
     geomagneticd \
     orientationd
 
